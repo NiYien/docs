d5bf070a46a2ddd01cc76390f57f20a39ed8aa7c
Jhe <362153154@qq.com>
2026-02-10 16:29:06 +0800

    update

--- api/telemetry-stats.js
+++ api/telemetry-stats.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.setHeader("Cache-Control", "no-store, max-age=0");
   if (req.method !== "GET") {
     res.setHeader("Allow", "GET");
     return res.status(405).json({ error: "Method Not Allowed" });
